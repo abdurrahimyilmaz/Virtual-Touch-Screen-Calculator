588c18d9f1acf0834d6192ffd1961b23eed942fd
abdurrahimyilmaz <abdurrahimyilmaz@users.noreply.github.com>
2022-11-02 14:16:31 +0100

    recovery files

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.sayacmetin = new System.Windows.Forms.TextBox();
+            this.alanmetin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.islemmetin = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.sonucmetin = new System.Windows.Forms.TextBox();
+            this.ilksayimetin = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.esittirmetin = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.sonsayimetin = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(965, 675);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // sayacmetin
+            // 
+            this.sayacmetin.Location = new System.Drawing.Point(355, 510);
+            this.sayacmetin.Name = "sayacmetin";
+            this.sayacmetin.Size = new System.Drawing.Size(213, 26);
+            this.sayacmetin.TabIndex = 1;
+            // 
+            // alanmetin
+            // 
+            this.alanmetin.Location = new System.Drawing.Point(355, 597);
+            this.alanmetin.Name = "alanmetin";
+            this.alanmetin.Size = new System.Drawing.Size(213, 26);
+            this.alanmetin.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(436, 487);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sayaç";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(444, 574);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Alan";
+            // 
+            // islemmetin
+            // 
+            this.islemmetin.Location = new System.Drawing.Point(354, 178);
+            this.islemmetin.Name = "islemmetin";
+            this.islemmetin.Size = new System.Drawing.Size(213, 26);
+            this.islemmetin.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(424, 143);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "İşlem";
+            // 
+            // sonucmetin
+            // 
+            this.sonucmetin.Location = new System.Drawing.Point(354, 443);
+            this.sonucmetin.MaxLength = 7;
+            this.sonucmetin.Name = "sonucmetin";
+            this.sonucmetin.Size = new System.Drawing.Size(213, 26);
+            this.sonucmetin.TabIndex = 15;
+            // 
+            // ilksayimetin
+            // 
+            this.ilksayimetin.Location = new System.Drawing.Point(354, 103);
+            this.ilksayimetin.Name = "ilksayimetin";
+            this.ilksayimetin.Size = new System.Drawing.Size(213, 26);
+            this.ilksayimetin.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(436, 401);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 20);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Sonuç";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(424, 74);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(59, 20);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "İlk Sayı";
+            // 
+            // esittirmetin
+            // 
+            this.esittirmetin.Location = new System.Drawing.Point(354, 347);
+            this.esittirmetin.Name = "esittirmetin";
+            this.esittirmetin.Size = new System.Drawing.Size(213, 26);
+            this.esittirmetin.TabIndex = 19;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(436, 307);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(49, 20);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Eşittir";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(424, 227);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(72, 20);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Son Sayı";
+            // 
+            // sonsayimetin
+            // 
+            this.sonsayimetin.Location = new System.Drawing.Point(354, 260);
+            this.sonsayimetin.Name = "sonsayimetin";
+            this.sonsayimetin.Size = new System.Drawing.Size(213, 26);
+            this.sonsayimetin.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 699);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.alanmetin);
+            this.Controls.Add(this.sayacmetin);
+            this.Controls.Add(this.sonsayimetin);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.esittirmetin);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.ilksayimetin);
+            this.Controls.Add(this.sonucmetin);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.islemmetin);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hesap Makinesi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox sayacmetin;
+        private System.Windows.Forms.TextBox alanmetin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox islemmetin;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox sonucmetin;
+        private System.Windows.Forms.TextBox ilksayimetin;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox esittirmetin;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox sonsayimetin;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,323 @@
+﻿
+//Abdurrahim Yılmaz 1606A041 a.rahim.yilmaz@hotmail.com
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using AForge.Math;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+                
+        private FilterInfoCollection cihazlar;     
+        private VideoCaptureDevice kamera;         
+
+        double? ilksayi = 0, sonsayi = 0;
+        string islem = "";
+        double? sonuc;
+        int sayac1 = 0;         //İlk sayıyı denetler.
+        int sayac = 0;          //Hesap makinesinin görünmesi için görüntüyü denetler.
+        int sayac2 = 0;         //İşlemin sonucu hesaplanıp hesaplanmadığını denetler.
+        string esittir;
+        string sonuconcesi;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+           
+        }       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            Control.CheckForIllegalCrossThreadCalls = false;                
+            cihazlar = new
+            FilterInfoCollection(FilterCategory.VideoInputDevice);
+            
+            kamera = new
+            VideoCaptureDevice(cihazlar[0].MonikerString);                 
+            
+            kamera.NewFrame += new NewFrameEventHandler(kamera_klonlama);                   
+            kamera.Start();            
+        }        
+
+        public void kamera_klonlama(object sender, NewFrameEventArgs eventArgs)      
+        {
+            Bitmap klon = (Bitmap)eventArgs.Frame.Clone();          //Yapılacak her faklı işlem için kameradan alınan görüntü bitmape atandı.
+            Bitmap cisim = (Bitmap)eventArgs.Frame.Clone();           
+            Bitmap asama = (Bitmap)eventArgs.Frame.Clone();
+
+            say(asama);         //Kameranın önüne gelindiğinde hesap makinesinin görünmesini denetleyen fonksiyon.
+
+            gorun(klon);        //Kameranın önüne gelindiğinde koşul sağlanırsa hesap makinesini gösterecek fonksiyon.
+
+            ColorFiltering filtre = new ColorFiltering();          //Kırmızı cisimin tespiti için renk filtreleme yapıldı.
+            filtre.Red = new IntRange(100, 255);            
+            filtre.Blue = new IntRange(0, 25);              
+            filtre.Green = new IntRange(0, 25);          
+            filtre.ApplyInPlace(cisim);
+
+            if (sayac == 1) anafonk(cisim);         //Hesap makinesinin fonksiyonu.Eşik geçilince görünecek.
+        }               
+
+        public void say(Bitmap image)
+        {
+            /*  
+             *  Denetleme işlemi için alınan görüntü üzerinde eşikleme yaparak gri kanalın
+             *  histogramının ortalaması ile bir eşik oluşturdum.Görüntüde beyaz haricinde ki
+             *  renkler ne kadar artarsa ortalama o kadar düşeceğinden belli bir sayının altından
+             *  sonrası için hesap makinesi görünüp işlem yapılabilecek.                           
+             */
+
+            Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);                
+            Bitmap grayImage = filter.Apply(image);
+            
+            Threshold filter1 = new Threshold(100);            
+            filter1.ApplyInPlace(grayImage);
+
+            ImageStatistics stat = new ImageStatistics(grayImage);            
+            Histogram gri = stat.Gray;
+
+            if (gri.Mean < 215) sayac = 1;
+            else sayac = 0;
+
+            string sayi = sayac.ToString();
+            sayacmetin.Text = sayi;
+        }
+
+        public void gorun(Bitmap image)
+        {           
+            // Hesap makinesi çerçevelerinin ekran üzerinde çizdirilmesi.
+             
+            Pen pen = new Pen(Color.Red);
+            Font font = new Font("Arial", 33);
+            Graphics g = Graphics.FromImage(image);             
+
+            if (sayac == 1)
+            {                
+                g.DrawRectangle(pen, 50, 45, 265, 370);
+
+                g.DrawRectangle(pen, 55, 50, 255, 100);
+
+                g.DrawRectangle(pen, 55, 155, 60, 60); g.DrawRectangle(pen, 120, 155, 60, 60); g.DrawRectangle(pen, 185, 155, 60, 60); g.DrawRectangle(pen, 250, 155, 60, 60);
+
+                g.DrawRectangle(pen, 55, 220, 60, 60); g.DrawRectangle(pen, 120, 220, 60, 60); g.DrawRectangle(pen, 185, 220, 60, 60); g.DrawRectangle(pen, 250, 220, 60, 60);
+
+                g.DrawRectangle(pen, 55, 285, 60, 60); g.DrawRectangle(pen, 120, 285, 60, 60); g.DrawRectangle(pen, 185, 285, 60, 60); g.DrawRectangle(pen, 250, 285, 60, 60);
+
+                g.DrawRectangle(pen, 55, 350, 60, 60); g.DrawRectangle(pen, 120, 350, 60, 60); g.DrawRectangle(pen, 185, 350, 60, 60); g.DrawRectangle(pen, 250, 350, 60, 60);
+
+                //      Çerçeve
+                //      Ekran
+                // 1    2    3    +
+                // 4    5    6    -
+                // 7    8    9    *
+                // 0    C    =    /
+
+                g.DrawString("1", font, Brushes.Red, 67, 162); g.DrawString("2", font, Brushes.Red, 132, 162); g.DrawString("3", font, Brushes.Red, 196, 162); g.DrawString("+", font, Brushes.Red, 259, 162);
+
+                g.DrawString("4", font, Brushes.Red, 67, 227); g.DrawString("5", font, Brushes.Red, 132, 227); g.DrawString("6", font, Brushes.Red, 196, 227); g.DrawString("-", font, Brushes.Red, 266, 227);
+
+                g.DrawString("7", font, Brushes.Red, 67, 292); g.DrawString("8", font, Brushes.Red, 132, 292); g.DrawString("9", font, Brushes.Red, 196, 292); g.DrawString("*", font, Brushes.Red, 265, 292);
+
+                g.DrawString("0", font, Brushes.Red, 67, 357); g.DrawString("C", font, Brushes.Red, 130, 357); g.DrawString("=", font, Brushes.Red, 196, 357); g.DrawString("/", font, Brushes.Red, 266, 357);
+            }
+            pictureBox1.Image = image;
+
+        }
+
+        public void anafonk(Bitmap image)
+        {
+            /*
+             *  Bu fonksiyon da kırmızı cismin tespit edilerek konum ve alan verisini kullanarak
+             *  belli eşiklerin üzerine geçtiğinde bulunduğu konumda ki sayıyı alacak veya işlemi
+             *  yapacak.
+             */
+
+            BlobCounter blobCounter = new BlobCounter();           
+            blobCounter.FilterBlobs = true;       
+            blobCounter.MinHeight = 7;        
+            blobCounter.MinWidth = 7;         
+            blobCounter.ObjectsOrder = ObjectsOrder.Size;
+
+            Font font = new Font("Arial", 15);
+            Graphics g = pictureBox1.CreateGraphics();
+            
+            Grayscale grayFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+            Bitmap grayImage = grayFilter.Apply(image);            
+            blobCounter.ProcessImage(grayImage);
+
+            Rectangle[] rects = blobCounter.GetObjectsRectangles();  
+
+            Font font2 = new Font("Arial", 33);
+            DateTime Zaman = DateTime.Now;
+
+            int denetim = (Zaman.Millisecond / 35);            
+            
+            if (rects.Length > 0)         
+            {
+                Rectangle objectRect = rects[0];                
+
+                Blob[] blobs = blobCounter.GetObjectsInformation();
+                int alan = blobs[0].Area;
+                if (alan > 700) alan = 701;
+                float noktaYY = blobs[0].CenterOfGravity.Y - 70;
+                float noktaY = blobs[0].CenterOfGravity.Y - 20;
+                float noktaX = blobs[0].CenterOfGravity.X - 20;
+                alanmetin.Text = alan.ToString();
+
+                AForge.Point nokta = new AForge.Point();
+                nokta = blobs[0].CenterOfGravity;                
+
+                Font yeni = new Font("Arial", 50);
+                g.DrawString(".", yeni, Brushes.Black, noktaX, noktaYY);
+
+                if (nokta.X < 115 && nokta.X > 55 && noktaY > 155 && noktaY < 205 && alan > 700 && denetim == 5)
+                {          
+                    if (sayac1 == 0) ilksayimetin.Text = ilksayimetin.Text + "1";
+                    else sonsayimetin.Text = sonsayimetin.Text + "1";
+                }
+
+
+                if (nokta.X < 180 && nokta.X > 120 && noktaY > 155 && noktaY < 205 && alan > 700 && denetim == 5)
+                {   
+                 
+                    if (sayac1 == 0) ilksayimetin.Text = ilksayimetin.Text + "2";
+                    else sonsayimetin.Text = sonsayimetin.Text + "2";
+                }
+
+                if (nokta.X < 245 && nokta.X > 185 && noktaY > 155 && noktaY < 205 && alan > 700 && denetim == 5)
+                {
+                    if (sayac1 == 0) ilksayimetin.Text = ilksayimetin.Text + "3";
+                    else sonsayimetin.Text = sonsayimetin.Text + "3";
+                }
+
+                if (nokta.X < 310 && nokta.X > 250 && noktaY > 155 && noktaY < 205 && alan > 700 && denetim == 5 && ilksayimetin.Text != "")
+                {
+                    islemmetin.Text = "+";
+                    ilksayi = Convert.ToDouble(ilksayimetin.Text);
+                    islem = "+";
+                    sayac1 = 1;
+                }
+
+                if (nokta.X < 115 && nokta.X > 55 && noktaY > 220 && noktaY < 280 && alan > 700 && denetim == 5)
+                {
+                    if (sayac1 == 0) ilksayimetin.Text = ilksayimetin.Text + "4";
+                    else sonsayimetin.Text = sonsayimetin.Text + "4";
+                }
+
+                if (nokta.X < 180 && nokta.X > 120 && noktaY > 220 && noktaY < 280 && alan > 700 && denetim == 5)
+                {
+                    if (sayac1 == 0) ilksayimetin.Text = ilksayimetin.Text + "5";
+                    else sonsayimetin.Text = sonsayimetin.Text + "5";
+                }
+
+                if (nokta.X < 245 && nokta.X > 185 && noktaY > 220 && noktaY < 280 && alan > 700 && denetim == 5)
+                {
+                    if (sayac1 == 0) ilksayimetin.Text = ilksayimetin.Text + "6";
+                    else sonsayimetin.Text = sonsayimetin.Text + "6";
+                }
+
+                if (nokta.X < 310 && nokta.X > 250 && noktaY > 220 && noktaY < 280 && alan > 700 && denetim == 5 && ilksayimetin.Text != "")
+                {
+                    islemmetin.Text = "-";
+                    ilksayi = Convert.ToDouble(ilksayimetin.Text);
+                    islem = "-";
+                    sayac1 = 1;
+                }
+
+                if (nokta.X < 115 && nokta.X > 55 && noktaY > 285 && noktaY < 345 && alan > 700 && denetim == 5)
+                {
+                    if (sayac1 == 0) ilksayimetin.Text = ilksayimetin.Text + "7";
+                    else sonsayimetin.Text = sonsayimetin.Text + "7";
+                }
+
+                if (nokta.X < 180 && nokta.X > 120 && noktaY > 285 && noktaY < 345 && alan > 700 && denetim == 5)
+                {
+                    if (sayac1 == 0) ilksayimetin.Text = ilksayimetin.Text + "8";
+                    else sonsayimetin.Text = sonsayimetin.Text + "8";
+                }
+
+                if (nokta.X < 245 && nokta.X > 185 && noktaY > 285 && noktaY < 345 && alan > 700 && denetim == 5)
+                {
+                    if (sayac1 == 0) ilksayimetin.Text = ilksayimetin.Text + "9";
+                    else sonsayimetin.Text = sonsayimetin.Text + "9";
+                }
+
+                if (nokta.X < 310 && nokta.X > 250 && noktaY > 285 && noktaY < 345 && alan > 700 && denetim == 5 && ilksayimetin.Text != "")
+                {
+                    ilksayi = Convert.ToDouble(ilksayimetin.Text);
+                    islemmetin.Text = "*";
+                    islem = "*";
+                    sayac1 = 1;
+                }
+
+                if (nokta.X < 115 && nokta.X > 55 && noktaY > 350 && noktaY < 410 && alan > 700 && denetim == 5)
+                {
+                    if (sayac1 == 0) ilksayimetin.Text = ilksayimetin.Text + "0";
+                    else sonsayimetin.Text = sonsayimetin.Text + "0";
+                }
+
+                if (nokta.X < 180 && nokta.X > 120 && noktaY > 350 && noktaY < 410 && alan > 700 && denetim == 5)
+                {
+                    islemmetin.Clear();                  
+                    ilksayimetin.Clear();
+                    sonucmetin.Clear();
+                    sonsayimetin.Clear();
+                    esittirmetin.Clear();
+                    sonuc = null;
+                }
+
+                if (nokta.X < 310 && nokta.X > 250 && noktaY > 350 && noktaY < 410 && alan > 700 && denetim == 5 && ilksayimetin.Text != "")
+                {
+                    islemmetin.Text = "/";
+                    ilksayi = Convert.ToDouble(ilksayimetin.Text);
+                    islem = "/";
+                    sayac1 = 1;
+                }                
+
+                if (nokta.X < 245 && nokta.X > 185 && noktaY > 350 && noktaY < 410 && alan > 700 && denetim == 5 && sonsayimetin.Text != "")
+                {
+
+                    esittirmetin.Text = "=";
+                    sonsayi = Convert.ToDouble(sonsayimetin.Text);
+                    ilksayimetin.Text = ilksayi.ToString();
+                    if (islem == "+")
+                    {
+                        sonuc = ilksayi + sonsayi;
+                    }
+                    if (islem == "-")
+                    {
+                        sonuc = ilksayi - sonsayi;
+                    }
+                    if (islem == "*")
+                    {
+                        sonuc = ilksayi * sonsayi;
+                    }
+                    if (islem == "/")
+                    {
+                        sonuc = ilksayi / sonsayi;
+                    }                   
+                    
+                    sayac1 = 0;
+                    sayac2 = 1;
+                }                 
+
+                esittir = sonuc.ToString();
+                sonuconcesi = ilksayimetin.Text + islemmetin.Text + sonsayimetin.Text + esittirmetin.Text;
+                sonucmetin.Text = ilksayimetin.Text + islemmetin.Text + sonsayimetin.Text + esittirmetin.Text + esittir;
+            }
+
+            if (sayac2 == 0) g.DrawString(sonuconcesi, font2, Brushes.Black, 60, 60);
+            else g.DrawString(sonucmetin.Text, font2, Brushes.Black, 60, 60);
+            
+        }        
+    }
+}
